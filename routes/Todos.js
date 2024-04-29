a69f6e92e9c77e1c3b6881b53dddca8bde3152b3
Tushar <kmrtushar01@gmail.com>
2024-04-29 21:31:24 +0530

    adding get request route

--- routes/Todos.js
+++ routes/Todos.js
@@ -2,7 +2,15 @@ const express = require("express")
 const router = express.Router();
 
 const {CreateTodo} = require('../controllers/CreateTodo');
+const {getTodos } = require('../controllers/getTodos');
+const {getTodoById} = require('../controllers/getTodoById');
+const {updateById} = require('../controllers/update');
+const {remove} = require('../controllers/delete');
 
 router.post("/createTodos", CreateTodo);
+router.get("/getTodos", getTodos);
+router.get("/id/:id", getTodoById);
+router.put("/update/:id", updateById);
+router.delete("/remove/:id", remove);
 
 module.exports = router;
