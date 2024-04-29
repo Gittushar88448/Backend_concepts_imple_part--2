ba5ec4237dc48b97247569c6abb1520b12a237da
Tushar <kmrtushar01@gmail.com>
2024-04-29 21:29:35 +0530

    writing port address on console

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const express = require('express')
 const app = express();
 
-app.listen(3000, ()=>{console.log("server start")});
+require('dotenv').config();
+const PORT = process.env.PORT || 8000
+
+// middleware to read request's body in the json format
+app.use(express.json());
+
+const todoRoutes = require("./routes/Todos");
+
+app.use("/api/v1",todoRoutes);
+
+app.listen(PORT , ()=>{
+    console.log(`connected established at port ${PORT}`);
+})
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get('/' , (req , res)=>{
+    res.send(`<h1>This is the homepage </h1>`);
+})
