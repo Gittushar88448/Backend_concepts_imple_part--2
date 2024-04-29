2d50a51226cafbc3ff996cd2c3f3484f9255f1a0
Tushar <kmrtushar01@gmail.com>
2024-04-29 21:33:54 +0530

    put request to update the existing data

--- routes/Todos.js
+++ routes/Todos.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const {CreateTodo} = require('../controllers/CreateTodo');
 const {getTodos } = require('../controllers/getTodos');
 const {getTodoById} = require('../controllers/getTodoById');
+const {updateById} = require('../controllers/update');
 
 router.post("/createTodos", CreateTodo);
 router.get("/getTodos", getTodos);
 router.get("/id/:id", getTodoById);
-
+router.put("/update/:id", updateById);
 
 module.exports = router;
