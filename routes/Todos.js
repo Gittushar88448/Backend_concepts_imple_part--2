429f0de808e85f72f10c1546f8bff320059bcc82
Tushar <kmrtushar01@gmail.com>
2024-04-29 21:35:33 +0530

    delete request to remove the data

--- routes/Todos.js
+++ routes/Todos.js
@@ -5,10 +5,12 @@ const {CreateTodo} = require('../controllers/CreateTodo');
 const {getTodos } = require('../controllers/getTodos');
 const {getTodoById} = require('../controllers/getTodoById');
 const {updateById} = require('../controllers/update');
+const {remove} = require('../controllers/delete');
 
 router.post("/createTodos", CreateTodo);
 router.get("/getTodos", getTodos);
 router.get("/id/:id", getTodoById);
 router.put("/update/:id", updateById);
+router.delete("/remove/:id", remove);
 
 module.exports = router;
